7db18f94396a0e7dd2c8468350f3a261d6d23820
Zihan Gao <cgao@lcs.on.ca>
2026-06-04 08:22:26 -0400

    Initial Commit

---
+++ ICS3UCulminating/ICS3UCulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3UCulminatingApp.swift
+//  ICS3UCulminating
+//
+//  Created by GaoZiHan on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct ICS3UCulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
